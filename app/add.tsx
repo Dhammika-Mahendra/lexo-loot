c4c2ef4e8bdf818ea84eac6d1acd7f0791175e27
Dhammika-Mahendra <wmdhammikamahendra@gmail.com>
2024-08-25 17:55:03 +0530

    Basic single record adding

--- app/add.tsx
+++ app/add.tsx
@@ -2,13 +2,26 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 import React from 'react'
 import { Colors } from '@/constants/Colors'
 import { TextInput } from 'react-native-gesture-handler'
+import { useSharedState } from '@/constants/Cntxt'
+import { useNavigation } from '@react-navigation/native'
 
 const add = () => {
+  const navigate=useNavigation()
+
+  const {word, setWord} = useSharedState()
+
+  const [newWord, setNewWord] = React.useState<string>('')
+  const addWord = () => {
+    setWord([...word, newWord])
+    setNewWord('')
+    navigate.navigate('index' as never)
+  }
+
   return (
     <View style={styles.container}>
       <Text>Add new word</Text>
-      <TextInput></TextInput>
-      <Button title="Add"></Button>
+      <TextInput onChangeText={(e)=>setNewWord(e)}></TextInput>
+      <Button title="Add" onPress={addWord}></Button>
     </View>
   )
 }
